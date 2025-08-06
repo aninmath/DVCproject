6a7188f72a0d9fa86874e82507e7da229c512184
Sarkar <anindya.sarkar@shell.com>
2025-08-06 15:43:10 +0530

    done

--- mera.py
+++ mera.py
@@ -1,3 +1,8 @@
 print ("hello world")
 
 #albal chal
+
+print ("hello world again")
+
+# This code creates a DataFrame, adds rows, and saves it to a CSV file.
+# The DataFrame contains names, ages, and cities of individuals.
